14f6ee56da341f2ebf17b130f4ab11075d9b7c72
bakerdh <55408223+bakerdh@users.noreply.github.com>
2022-05-24 10:42:35 +0100

    Update renameandupload.R

--- renameandupload.R
+++ renameandupload.R
@@ -2,7 +2,7 @@
 # also re-orders by AQ score, and uploads data to OSF
 # DHB 23/5/22
 
-packagelist <- c('remotes','tictoc','utils')
+packagelist <- c('remotes','tictoc','utils','R.matlab')
 missingpackages <- packagelist[!packagelist %in% installed.packages()[,1]]
 if (length(missingpackages)>0){install.packages(missingpackages)}
 if (packagelist[!'osfr' %in% installed.packages()[,1]]){remotes::install_github("centerforopenscience/osfr")}
@@ -10,8 +10,9 @@ packagelist <- c(packagelist,'osfr')
 toinstall <- packagelist[which(!packagelist %in% (.packages()))]
 invisible(lapply(toinstall,library,character.only=TRUE))
 
+my_wd <- getwd()
 
-osf_auth(token = '3dEYuhZNmwWbG3xuhRIiVRo0T2oniOkEP3Ip8i1LPG4PNjeTRln54eGNAG7oyTT9xozWwJ')
+osf_auth(token = '')
 osfproject <- osf_retrieve_node("ab3yv")
 componentlist <- osf_ls_nodes(osfproject)
 EEGID <- pmatch('Raw EEG data 1',as.character(unlist(componentlist[,1])))
@@ -20,6 +21,8 @@ EEGfilesLow <- osf_ls_files(EEGtokenLow,n_max=300)
 EEGID <- pmatch('Raw EEG data 2',as.character(unlist(componentlist[,1])))
 EEGtokenHi <- componentlist[EEGID,2]
 EEGfilesHi <- osf_ls_files(EEGtokenHi,n_max=300)
+
+rawdatadir <- '~/Desktop/UG2021/'
 
 rawdata <- read.csv('~/Google Drive/Current work/Teaching/BSc projects 2021/ASD/BSc project questionnaires 2021 (Responses) - Form Responses 1.csv')
 
@@ -43,41 +46,63 @@ uniqueID[85] <- '2705b'
 QIDs[83] <- '2705b'
 QIDs[53] <- '5417'
 
-IDmatching <- data.frame(uniqueID,QIDs)
-
-# these are the indices in the questionnaire data for each EEG dataset
-Qindices <- NULL
-for (n in 1:nrow(IDmatching)){
-  Qindices[n] <- match(IDmatching[n,1],IDmatching[,2])
-}
-
-Eindices <- which(!is.na(Qindices))
-orderedAQ <- qdata$AQ[Qindices]
-
-subjorder <- sort(orderedAQ,index.return=TRUE)
-
-
-for (s in 1:length(Eindices)){
-  if (s!=57){    # subject 57 did not complete the experiment so is omitted
-   
+subjcount <- 0
+AQinsubjorder <- NULL
+for (s in 1:length(uniqueID)){
+  if (s!=58){    # subject 58 did not complete the questionnaire so is omitted
+  subjcount <- subjcount + 1
   thisIDnumber <- uniqueID[s]  
-  QIDnumber <- match(thisIDnumber,QIDs)  
-  qresponses <- rawdata[QIDnumber,]
+  QIDnumber <- match(thisIDnumber,QIDs) 
   qscores <- qdata[QIDnumber,]
-  
-  targetparticipant <- subjorder$ix[s]
-  
-  
-  thisAQ <- orderedAQ[targetparticipant]
-  qdata$AQ[Eindices[targetparticipant]]
-  IDmatching[targetparticipant,]
-  
-  
-  
+  AQinsubjorder[subjcount] <- qscores$AQ
   }
 }
 
+newsubjorder <- sort(AQinsubjorder,index.return=TRUE)
 
+subjcount <- 0
+for (s in 1:length(uniqueID)){
+  if (s!=58){    # subject 58 did not complete the experiment so is omitted
+      print(s)
+      
+      setwd(my_wd)
+      subjcount <- subjcount + 1
+      thisIDnumber <- uniqueID[s]  
+      QIDnumber <- match(thisIDnumber,QIDs)  
+      qresponses <- rawdata[QIDnumber,3:148]
+      
+      newpno <- which(newsubjorder$ix==subjcount)
+      
+      csvfiles <- dir(paste0(rawdatadir,'P',s+200),pattern='csv.gz',full.names=TRUE)
+      
+      newdir <- paste0('temp/raw/S',newpno)
+      dir.create(newdir)
+      for (f in 1:length(csvfiles)){
+      file.copy(csvfiles[f],paste0(newdir,'/Block',f,'.csv.gz'))
+      }
+      write.csv(qresponses,file=paste0(newdir,'/questionnairedata.csv'),row.names=FALSE)
+      
+      setwd('temp/raw/')
+      d <- dir(paste0('S',newpno))
+      zip(paste0('S',newpno,'.zip'),files=paste0('S',newpno,'/',d))
+      setwd(my_wd)
+      
+  }
+}
 
+# upload first 49 participants to the low AQ folder
+for (s in 1:49){
+  print(s)
+  osf_upload(EEGtokenLow,paste0('temp/raw/S',s,'.zip'),progress=TRUE)
+}
 
+# upload remaining 51 participants to the high AQ folder
+for (s in 50:100){
+  print(s)
+  osf_upload(EEGtokenHi,paste0('temp/raw/S',s,'.zip'),progress=TRUE)
+}
 
+for (s in 1:100){
+  print(s)
+  file.remove(paste0('temp/raw/S',s,'.zip'))
+}
